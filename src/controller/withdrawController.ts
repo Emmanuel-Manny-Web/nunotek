0bcf23866fec3b9e658ae4c9592995c264985ae3
Emmanuel-Manny-Web <ipaliboha@gmail.com>
2024-05-03 14:03:14 +0100

    blocked withdrawal

--- src/controller/withdrawController.ts
+++ src/controller/withdrawController.ts
@@ -62,79 +62,76 @@ export default class Handler {
     if (count < 2) {
       const withdraw = await Withdraw.find({ email: users.email, status: "Pending" })
       const investments = await Investment.find({ user: users.email, active: true })
-      if (withdraw.length <= 0 || withdraw.length >= 0) {
-        res.status(201).json({ ok: false, error: "Withdrawal is currently unable to be processed" })
-      } else {
-        if (investments.length > 0) {
-          if(withdraw.length < 1) {
-            try {
-              if(account) {
-                if (amount >= 1000 && amount <= 500000) {
-                  if (wallet.profit >= parseInt(amount)) {
-                    var deduct = 0.05 * parseInt(amount)
-                    var paid = parseInt(amount) - deduct
-                    if(!users.suspend) {
-                      const payload = {
-                        account_bank: account.code,
-                        account_number: account.accountnumber,
-                        amount: paid,
-                        currency: "NGN",
-                        narration: "Nunotek Payment",  
-                        reference: `TRF-${Date.now()}`
-                      }
-                      const response = await flw.Transfer.initiate(payload)
-                      const { data } = response
-                      setTimeout(async () => {
-                        if (data !== null) {
-                          const { data: result } = await flw.Transfer.get_a_transfer({ id: JSON.stringify(data.id) })
-                          await Wallet.findOneAndUpdate({ email: wallet.email }, {
-                            profit: wallet.profit - parseInt(amount)
-                          })
-                          if(result.status === "SUCCESSFUL") {
-                            await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, status: "Paid", withdrawalID: JSON.stringify(data.id) })
-                            await Notification.create({ email: wallet.email, amount: paid, message: `Successful withdrawal of ₦${Number(paid).toLocaleString()}` })
-                            res.status(200).json({ ok: true, message: "Withdrawal has been made successfully." })
-                          } else if(result.status === 'FAILED') {
-                            await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, withdrawalID: JSON.stringify(data.id), status: "Declined" })
-                            await Notification.create({ email: wallet.email, amount: paid, message: `Declined withdrawal of ₦${Number(paid).toLocaleString()}` })
-                            await Wallet.findOneAndUpdate({ email: wallet.email }, {
-                              $inc: {
-                                profit: amount
-                              }
-                            })
-                            res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
-                          } else {
-                            await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, withdrawalID: JSON.stringify(data.id) })
-                            res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
-                          } 
-                        } else {
-                          await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, status: "Declined" })
-                          await Notification.create({ email: wallet.email, amount: paid, message: `Failed to process withdrawal of ₦${Number(paid).toLocaleString()}` })
-                          res.status(200).json({ ok: false, error: "Error processing request" })
-                        }
-                      }, 1000)
-                    } else {
-                      await Wallet.findOneAndUpdate({ email: wallet.email }, {
-                        profit: wallet.profit - parseInt(amount)
-                      })
-                      await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct })
-                      res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
-                    }
-                  } else res.status(201).json({ ok: false, error: "Insufficient profit balance, purchase an investment to earn profit." })
-                } else {
-                  res.status(201).json({ ok: false, error: "Minimum withdrawal of 1000 and maximum 500,000"})
-                }
+      if (investments.length > 0) {
+        if(withdraw.length < 1) {
+          try {
+            if(account) {
+              if (amount >= 1000 && amount <= 500000) {
+                if (wallet.profit >= parseInt(amount)) {
+                  // var deduct = 0.05 * parseInt(amount)
+                  // var paid = parseInt(amount) - deduct
+                  // if(!users.suspend) {
+                  //   const payload = {
+                  //     account_bank: account.code,
+                  //     account_number: account.accountnumber,
+                  //     amount: paid,
+                  //     currency: "NGN",
+                  //     narration: "Nunotek Payment",  
+                  //     reference: `TRF-${Date.now()}`
+                  //   }
+                  //   const response = await flw.Transfer.initiate(payload)
+                  //   const { data } = response
+                  //   setTimeout(async () => {
+                  //     if (data !== null) {
+                  //       const { data: result } = await flw.Transfer.get_a_transfer({ id: JSON.stringify(data.id) })
+                  //       await Wallet.findOneAndUpdate({ email: wallet.email }, {
+                  //         profit: wallet.profit - parseInt(amount)
+                  //       })
+                  //       if(result.status === "SUCCESSFUL") {
+                  //         await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, status: "Paid", withdrawalID: JSON.stringify(data.id) })
+                  //         await Notification.create({ email: wallet.email, amount: paid, message: `Successful withdrawal of ₦${Number(paid).toLocaleString()}` })
+                  //         res.status(200).json({ ok: true, message: "Withdrawal has been made successfully." })
+                  //       } else if(result.status === 'FAILED') {
+                  //         await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, withdrawalID: JSON.stringify(data.id), status: "Declined" })
+                  //         await Notification.create({ email: wallet.email, amount: paid, message: `Declined withdrawal of ₦${Number(paid).toLocaleString()}` })
+                  //         await Wallet.findOneAndUpdate({ email: wallet.email }, {
+                  //           $inc: {
+                  //             profit: amount
+                  //           }
+                  //         })
+                  //         res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
+                  //       } else {
+                  //         await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, withdrawalID: JSON.stringify(data.id) })
+                  //         res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
+                  //       } 
+                  //     } else {
+                  //       await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct, status: "Declined" })
+                  //       await Notification.create({ email: wallet.email, amount: paid, message: `Failed to process withdrawal of ₦${Number(paid).toLocaleString()}` })
+                  //       res.status(200).json({ ok: false, error: "Error processing request" })
+                  //     }
+                  //   }, 1000)
+                  // } else {
+                  //   await Wallet.findOneAndUpdate({ email: wallet.email }, {
+                  //     profit: wallet.profit - parseInt(amount)
+                  //   })
+                  //   await Withdraw.create({ email: wallet.email, amount: paid, charge: deduct })
+                  //   res.status(200).json({ ok: true, message: "Withdrawal request has been made, payment will be received shortly." })
+                  // }
+                  res.status(200).json({ ok: false, error: "Withdrawal is currently unable to be processed"})
+                } else res.status(201).json({ ok: false, error: "Insufficient profit balance, purchase an investment to earn profit." })
               } else {
-                res.status(201).json({ ok: false, error: "You must bind an account to enable withdrawal"})
+                res.status(201).json({ ok: false, error: "Minimum withdrawal of 1000 and maximum 500,000"})
               }
-            } catch(err: any) {
-              res.status(201).json({ ok: false, error: err.message })
+            } else {
+              res.status(201).json({ ok: false, error: "You must bind an account to enable withdrawal"})
             }
-          } else {
-            res.status(201).json({ ok: false, error: "You have a pending withdrawal!" })
+          } catch(err: any) {
+            res.status(201).json({ ok: false, error: err.message })
           }
-        } else res.status(200).json({ ok: false, error: "Purchase an investment plan to enable withdrawal" }) 
-      }
+        } else {
+          res.status(201).json({ ok: false, error: "You have a pending withdrawal!" })
+        }
+      } else res.status(200).json({ ok: false, error: "Purchase an investment plan to enable withdrawal" }) 
     } else {
       res.status(200).json({ ok: false, error: "Maximum withdrawal of twice a day" })
     }
